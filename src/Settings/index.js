e991a892a096b343daa4a2a8b76708ba795ef09d
codenamics <info@codenamics.pl>
2018-11-09 23:39:22 +0100

    coingrid

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import Welcome from "./Welcome";
 import ConfirmButton from "./ConfirmButton";
 import Page from "../layout/Page";
+import CoinGrid from "./CoinGrid";
 export default function() {
   return (
     <Page name="settings">
       <Welcome />;
       <ConfirmButton />
+      <CoinGrid />
     </Page>
   );
 }
